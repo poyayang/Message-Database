68fd6f7c2aafaf3f09f8bc876b06ebf508bc3e7a
PoYa <poya.yang@icloud.com>
2023-11-02 23:24:40 +0000

    Initial structure

--- app.py
+++ app.py
@@ -3,14 +3,18 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-
 @app.route("/sms", methods=["Post"])
 def sms():
-    receive_body = request.form["Body"].upper()
-    message = receive_body.split()
+    receive_body = request.form["Body"]
+    message = receive_body.upper().split()
+    second_receive_body=request.form["Body"].upper()
     if message[0] == "SAVE":
         print("OK")
-    elif message[0] == "READ":
-        print(receive_body[5:])
+    elif message[0] == "SAVE" and second_receive_body=="READ":
+        print (receive_body[5:])
     else:
         print("Something might be wrong, please try again!")
+
+if __name__=='__main__': 
+    app.run(host='0.0.0.0', port=80)
+    app.app_context().push()
