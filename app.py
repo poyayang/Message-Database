1633f6c10b82994f5e0928449cff217a09fe4986
PoYa <poya.yang@icloud.com>
2023-11-06 23:30:25 +0000

    Bind parameter to the function
    use 'global'

--- app.py
+++ app.py
@@ -3,18 +3,36 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+
+
+
 @app.route("/sms", methods=["Post"])
 def sms():
     receive_body = request.form["Body"]
     message = receive_body.upper().split()
-    second_receive_body=request.form["Body"].upper()
+
     if message[0] == "SAVE":
-        print("OK")
-    elif message[0] == "SAVE" and second_receive_body=="READ":
-        print (receive_body[5:])
+        save_message(output=receive_body[5:])
+        return 'Message saved'
+    elif message[0] == "READ":
+        response = read_message()
+        return response
     else:
         print("Something might be wrong, please try again!")
 
-if __name__=='__main__': 
+
+message_stored=""
+
+def save_message(output: str):
+
+    global message_stored 
+    message_stored = output
+
+def read_message() -> str:
+
+    global message_stored 
+    return message_stored
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
     app.app_context().push()
