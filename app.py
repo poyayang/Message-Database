b53e492503ef16d9fd721db500577fb47604f8cf
PoYa <poya.yang@icloud.com>
2023-11-08 22:30:42 +0000

    Save message to dictionary

--- app.py
+++ app.py
@@ -1,37 +1,33 @@
 from twilio.twiml.messaging_response import MessagingResponse
 from flask import Flask, request
+from message import save_message, read_message
 
 app = Flask(__name__)
 
 
-
-
 @app.route("/sms", methods=["Post"])
 def sms():
-    receive_body = request.form["Body"]
+    phone_number = request.form['From']
+    receive_body = request.form.get("Body", type=str)
+    # Getting received message content
     message = receive_body.upper().split()
+    # Split the body for further use
 
+    resp = MessagingResponse()
+    # To send send the message back to Twilio
     if message[0] == "SAVE":
-        save_message(output=receive_body[5:])
-        return 'Message saved'
+        # If the first word is "SAVE",
+        save_message(number=phone_number, output=receive_body[5:])
+        # The message will be save from the 6 digits
+        return resp == 'Message saved'
     elif message[0] == "READ":
-        response = read_message()
-        return response
+        # If the first word is "READ",
+        response = read_message(number=phone_number)
+        # The message will be read from the storage
+        return resp == response
     else:
         print("Something might be wrong, please try again!")
 
-
-message_stored=""
-
-def save_message(output: str):
-
-    global message_stored 
-    message_stored = output
-
-def read_message() -> str:
-
-    global message_stored 
-    return message_stored
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
---
+++ message.py
@@ -0,0 +1,8 @@
+message_database={}
+
+def save_message(number: str, output: str):
+    message_database[number] = output
+    # The outcome of "output" will then be saved in the "message_stored"
+
+def read_message(number:str) -> str:
+    return message_database[number]
